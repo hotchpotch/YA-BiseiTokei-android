f5d6b466d1b732c03aa39c00da0beb5036d5a3d2
Yuichi Tateno <hotchpotch@gmail.com>
2010-02-10 20:53:29 +0900

    タスクを分離

--- src/in/hotchpotch/android/ImageLoader.java
+++ src/in/hotchpotch/android/ImageLoader.java
@@ -25,6 +25,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 
 import android.os.Handler;
+import android.os.Message;
 
 import android.util.Log;
 
@@ -40,71 +41,52 @@ public class ImageLoader extends View {
     public ImageLoader(Context context) {
         super(context);
         setBackgroundColor(Color.BLACK);
-        // Resources r = context.getResources();
-        updateImage();
         startTimer();
     }
 
     private void startTimer() {
         Timer timer = new Timer(true);
-        final Handler handler = new Handler();
-        timer.schedule(
-            new TimerTask() {
-                @Override
-                public void run() {
-                    handler.post( new Runnable(){
-                        public void run(){
-                            // Log.d(TAG, "update mImage");
-                            ImageLoader.this.updateImage();
-                        }
-                    });
-                }
-            }, 0, 1000);
+        Handler handler = new Handler() {
+            public void handleMessage(Message msg) {
+                ImageLoader.this.updateImage((String) msg.obj);
+            }
+        };
+        timer.schedule(new ImageUpdateTask(handler), 0, 1000);
     }
 
-    private void updateImage() {
-        String str = this.getPhotoPath();
-        // Log.d(TAG, str);
-        if (!str.equals(mLastPhotoPath)) {
-            // Log.d(TAG, "UPDATTTTTTTTTTTTTT");
-            mLastPhotoPath = str;
-            mImage = null;
-            mExecutor.execute(new Runnable() { public void run() {
-                    if (Thread.interrupted()) {
-                        return;
+    private void updateImage(String time) {
+        mImage = null;
+        final String path = getPhotoPath(time);
+        mExecutor.execute(new Runnable() { public void run() {
+            if (Thread.interrupted()) {
+                return;
+            }
+            InputStream fis = null;
+            try {
+                fis = new BufferedInputStream(new FileInputStream(path));
+                mImage = BitmapFactory.decodeStream(fis);
+                post(new Runnable() { 
+                    public void run() {
+                        invalidate();
                     }
-                    InputStream fis = null;
+                });
+            } catch (IOException e) {
+                Log.e(TAG, String.format("error file, %s", e.getMessage()), e);
+            } finally {
+                if (fis != null) {
                     try {
-                        fis = new BufferedInputStream(new FileInputStream(mLastPhotoPath));
-                        mImage = BitmapFactory.decodeStream(fis);
-                        post(new Runnable() { 
-                            public void run() {
-                                invalidate();
-                            }
-                        });
+                        fis.close();
                     } catch (IOException e) {
                         Log.e(TAG, String.format("error file, %s", e.getMessage()), e);
-                    } finally {
-                        if (fis != null) {
-                            try {
-                                fis.close();
-                            } catch (IOException e) {
-                                Log.e(TAG, String.format("error file, %s", e.getMessage()), e);
-                            }
-                        }
                     }
                 }
-            });
+            }
         }
+        });
     }
 
-    private String getPhotoPath() {
-        Calendar cal = Calendar.getInstance();
-        return String.format("%sPhotos/%02d%02d.jpg", 
-                BISEI_APP_DIR, 
-                cal.get(Calendar.HOUR_OF_DAY),
-                cal.get(Calendar.MINUTE)
-        );
+    private String getPhotoPath(String time) {
+        return String.format("%sPhotos/%s.jpg", BISEI_APP_DIR, time);
     }
 
   @Override
---
+++ src/in/hotchpotch/android/ImageUpdateTask.java
@@ -0,0 +1,35 @@
+package in.hotchpotch.android;
+
+import java.util.Calendar;
+import java.util.TimerTask;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class ImageUpdateTask extends TimerTask {
+    private String mLastTime;
+    private Handler updateHandler;
+
+    public ImageUpdateTask(Handler handler) {
+        super();
+        updateHandler = handler;
+    }
+
+    @Override
+    public void run() {
+        String currentTime = getHHMM();
+        if (!currentTime.equals(mLastTime)) {
+            mLastTime = currentTime;
+            Message msg = new Message();
+            msg.obj = mLastTime;
+            updateHandler.sendMessage(msg);
+        }
+    }
+
+    public String getHHMM() {
+        Calendar cal = Calendar.getInstance();
+        return String.format("%02d%02d", cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE));
+    }
+}
+
+
